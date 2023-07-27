a49110eb77e87f64dcbb6b438c02a2cc5f034a54
kalan <kalan@KALANITHI>
2023-07-27 14:07:33 +0530

    sfv

---
+++ operator/src/operator/Basic.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int add;
+		a=10;
+		b=20;
+		add=a+b;
+		System.out.println(add);
+
+	}
+
+}
